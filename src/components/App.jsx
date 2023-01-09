0472a45a61050da6f56246bcf81fc8ccacf45401
bmv000 <106543741+bmv000@users.noreply.github.com>
2023-01-09 22:11:43 +0200

    add  LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,6 +49,20 @@ class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const filterContacts = this.handleFilterContacts();
     return (
