45e158793f685fb8a20d66cea44469ca5a2a0eee
bmv000 <106543741+bmv000@users.noreply.github.com>
2023-02-22 22:56:08 +0200

    fixed styled

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,8 +2,14 @@ import imageHome from './imageHome.jpg';
 import  css  from './HomePage.module.css';
 const HomePage = () => {
   return (
-    <div className={css.container}>
-      <img src={imageHome} alt="Phone Book" width="100%" height="100%" />
+    <div className={css.home__page}>
+      <img
+        className={css.img}
+        src={imageHome}
+        alt="Phone Book"
+        width="100%"
+        height="850px"
+      />
     </div>
   );
 };
