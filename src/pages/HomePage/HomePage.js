5490add074668b751a1593f291b0bf9d6ac77e8c
bmv000 <106543741+bmv000@users.noreply.github.com>
2023-02-22 22:19:14 +0200

    Update HomePage.js

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import  css  from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={css.container}>
-      <img src={imageHome} alt="Phone Book" width="100%" height="320px" />
+      <img src={imageHome} alt="Phone Book" width="100%" height="100%" />
     </div>
   );
 };
