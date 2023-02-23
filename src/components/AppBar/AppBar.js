bc46b04b7950ce7fb7349ca93edf33e3a5ac45dd
bmv000 <106543741+bmv000@users.noreply.github.com>
2023-02-23 23:21:58 +0200

    fixed

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,16 +1,16 @@
-import { Navigation } from '../Navigation/Navigation';
-import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
-import { useAuth } from 'hooks';
-// import css from './AppBar.module.css';
+// import { Navigation } from '../Navigation/Navigation';
+// import { UserMenu } from '../UserMenu/UserMenu';
+// import { AuthNav } from '../AuthNav/AuthNav';
+// import { useAuth } from 'hooks';
+// // import css from './AppBar.module.css';
 
-export const AppBar = () => {
-  const { isLoggedIn } = useAuth();
+// export const AppBar = () => {
+//   const { isLoggedIn } = useAuth();
 
-  return (
-    <header className={css.header}>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
-  );
-};
+//   return (
+//     <header className={css.header}>
+//       <Navigation />
+//       {isLoggedIn ? <UserMenu /> : <AuthNav />}
+//     </header>
+//   );
+// };
